1285d3cb64ad8a217ce142d418b0991f9e2d4f01
znmop <zacharyprado1@gmail.com>
2019-09-05 16:38:35 -0700

    basic lexing done for now

--- Simply KoolB/Source/Compiler/main1.cpp
+++ Simply KoolB/Source/Compiler/main1.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <windows.h>
 
+#include "KoolBRead.hpp"
+
 
 void Start();
-void Compile();
+void Compile(int argc, char* argv[]);
 void Stop();
 
 DWORD StartTime;
@@ -14,22 +16,60 @@ DWORD StartTime;
 int main(int argc, char *argv[]) {
     std::cout << "Welcome to the Simply KoolB Compiler!" << std::endl;
     Start();
-    Compile();
+    Compile(argc, argv);
     Stop();
     return 0;
 }
+
 
 void Start() {
     StartTime = GetTickCount();
     return ;
 }
 
-void Compile() {
+
+void Compile(int argc, char* argv[]) {
+    if (argc != 2) {
+       std::cout << "Usage: KoolB <filename>" << std::endl;
+       exit(1);
+    }
+
+    Reading Read = Reading();
+
+    // Open the file, pass it to OpenBook()
+    Read.OpenBook(argv[1]);
+    Read.GetNextWord();
+
+    while (Read.WordType() != Read.None) {
+        // Loop through each word and print out its type.
+        switch (Read.WordType()) {
+        case Read.Identifier:
+            std::cout << "Found Identifier:\t\t" << Read.Word() << std::endl;
+            break;
+        case Read.String:
+            std::cout << "Found String:\t\t" << Read.Word() << std::endl;
+            break;
+        case Read.Symbol:
+            std::cout << "Found Symbol:\t\t" << Read.Word() << std::endl;
+            break;
+        case Read.EndOfLine:
+            // std::cout << "Found EndOfLine:\t\t" << Read.Word() << std::endl;
+            break;
+        default:
+            std::cout << "ERR: Unable to report what happened." << std::endl;
+            break;
+        }
+        // Sleep for 150ms then get the next word.
+        Sleep(150);
+        Read.GetNextWord();
+    }
     return ;
 }
+
+
 void Stop() {
     std::cout << "Compile Time:\t"
-              << static_cast<float>((GetTickCount()-StartTime) / 1000.0)
+              << static_cast<float>((GetTickCount() - StartTime) / 1000.0)
               << std::endl;
     return ;
 }
